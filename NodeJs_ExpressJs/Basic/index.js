81024251c07d9ca4eee6b9a8dc2224aebb8f27da
anjali-2511 <anjalimalharishinde2511@gmail.com>
2026-09-15 11:36:06 +0530

    Routes Completed

--- NodeJs_ExpressJs/Basic/index.js
+++ NodeJs_ExpressJs/Basic/index.js
@@ -9,7 +9,31 @@ app.listen(3000, ()=>{
     console.log('Successfully Connected on port 3000.')
 })
 
+// app.get('/', (req, res) => {
+//     res.send("<h1>Hello Anjali</h1>")
+// })
+
+
 app.get('/', (req, res) => {
-    res.send("<h1>Hello Anjali</h1>")
+    res.send("<h1>Welcome to Home Page</h1>")
 })
+
+app.get('/about', (req, res) => {
+    res.send("<h1>Welcome to About Page</h1>")
+})
+
+app.get('/about/user', (req, res) => {
+    res.send("<h1>Welcome to User Page</h1>")
+})
+
+app.get('/:userid-:bookname', (req,res) => {
+    res.send(req.params)
+})
+
+app.get('/search', (req,res) => {
+    const name = req.query.name
+    const age = req.query.age
+    res.send(`Search results for Name : ${name} , Age: ${age}`)
+})
+
 
