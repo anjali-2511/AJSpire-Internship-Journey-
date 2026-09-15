0823193714c228d17e9f1489292df5934e9b6422
anjali-2511 <anjalimalharishinde2511@gmail.com>
2026-09-15 11:15:02 +0530

    Fisrt code of expressjs

--- NodeJs_ExpressJs/Basic/index.js
+++ NodeJs_ExpressJs/Basic/index.js
@@ -1,2 +1,15 @@
-
+// Nodejs:
 console.log("object");
+
+// ExpressJs: 
+const express = require('express')
+const app = express()
+
+app.listen(3000, ()=>{
+    console.log('Successfully Connected on port 3000.')
+})
+
+app.get('/', (req, res) => {
+    res.send("<h1>Hello Anjali</h1>")
+})
+
