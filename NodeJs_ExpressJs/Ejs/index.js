a7ab1024ccfb7a5dd5715b9e49cd1f0c5c72b2e3
anjali-2511 <anjalimalharishinde2511@gmail.com>
2026-09-17 11:56:28 +0530

    Templates completed

--- NodeJs_ExpressJs/Ejs/index.js
+++ NodeJs_ExpressJs/Ejs/index.js
@@ -21,6 +21,29 @@ app.get('/about', (req,res)=>{
     res.render("about", {title:'About page', message:" Welcome" , items, objects:fruits})
 })
 
+app.use(express.urlencoded({extended: false}))
+app.get('/form', (req, res) =>{
+    res.render('form',{message: null})
+})
+
+app.post('/submit', (req, res) =>{
+    const name = req.body.myname
+
+    const message = `Hello, ${name} You submitted the form.`
+     res.render('form', {message:message})
+})
+
+app.get('/home', (req,res)=>{
+    let items=['Apple', 'Banana' , 'Cherry' , 'Orange']
+    let fruits = [
+            { name: "Apple", color: "Red", bg: "Red"},
+            { name: "Banana", color: "Yellow", bg: "Red" },
+            { name: "Grape", color: "Purple", bg: "Red" },
+            { name: "Orange", color: "Orange", bg: "Red" }
+        ];
+    res.render("home", {title:'Home page', message:" Welcome" , items, objects:fruits})
+})
+
 
 app.listen(3000,()=>{
     console.log('Successfully Connected on port 3000.')
